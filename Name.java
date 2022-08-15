6949e3b12cca4fd9fe19b7f06c63e624fa71743a
shubh2109 <shubham.bidarahalli@publicissapient.com>
2022-08-15 13:45:35 +0530

    files committed

---
+++ Name.java
@@ -0,0 +1,11 @@
+public class Name{
+    public static void main(String args[]){
+        System.out.println(" SSS      A  ");
+        System.out.println("S   S    A  A ");
+        System.out.println("S       A    A");
+        System.out.println(" SSS    AAAAAA");
+        System.out.println("    S   A    A");
+        System.out.println("S   S   A    A");
+        System.out.println(" SSS    A    A");
+    }
+}
